417c550000886051f498d379549d645dd9bb592e
Sachinm78 <sachinm78@gmail.com>
2019-04-03 12:44:19 -0700

    final clean-up of code

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,17 +6,19 @@ class Cli
 
     def run
         header
+        
         puts ""
         puts "Here is a list of current players.".colorize(:color => :white, :background => :red).bold
         puts ""
+        
         Scraper.scrape_blackhawks_roster
         display_player_names
+        
         puts ""
         menu                   
     end
 
     def display_player_names
-             
         Players.all.each.with_index(1) do |name, index|
             puts "  #{index}. #{name.name}".bold
         end
@@ -25,6 +27,7 @@ class Cli
        
     def menu
         puts "Please enter a number from the list below for more information.".colorize(:color => :white, :background => :red).bold
+        
         input = gets.chomp
         player = Players.all[input.to_i - 1]
             if !player
@@ -45,8 +48,8 @@ class Cli
                 puts "Bio:".colorize(:color => :white, :background => :red).bold
                 puts ""
                 puts "#{player.bio}".colorize(:color => :white, :background => :red).bold
-                puts "
-                "
+                puts ""
+
                 display_player_names
                 puts ""
                 choice
@@ -55,6 +58,7 @@ class Cli
     
     def choice
         puts "Would you like to see another player's info? (y/n)".colorize(:color => :white, :background => :red).bold
+        
         input = gets.chomp
         if input == "y"
             puts ""
